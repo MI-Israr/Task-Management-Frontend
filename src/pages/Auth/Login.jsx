f3a6d7403fcd1f7cf0b7f47919a6fc4371576c15
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-01-23 19:06:05 +0500

    creating login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
 
 const Login = () => {
-  return <div>Login</div>;
+  return <AuthLayout>Login</AuthLayout>;
 };
 
 export default Login;
