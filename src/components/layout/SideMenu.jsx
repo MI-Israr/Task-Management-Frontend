8cac58c526736960c093da878961d12a17d2b57a
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-06 22:32:19 +0500

    sidebar

--- src/components/layout/SideMenu.jsx
+++ src/components/layout/SideMenu.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
-const SideMenu = () => {
-  return <div>SideMenu</div>;
+const SideMenu = ({ activeMenu }) => {
+  const { user, clearUser } = useContext(UserContext);
+  const [sideMenuData, setSideMenuData] = useState([]);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (route === "logout") {
+      handleLogout();
+      return;
+    }
+    navigate(route);
+  };
+
+  const handleLogout = () => {
+    localStorage.clear();
+    clearUser();
+    navigate("/login");
+  };
+
+  useEffect(() => {
+    if(user){
+      setSideMenuData(user?.role==='admin' ? SIDE_MENU_DATA : SIDE_MENU_USER_DATA)
+    }
+  }, [])
+  
+
+  return <div></div>;
 };
 
 export default SideMenu;
