9504824d51b987dd38865cc5b7d09038b2785f7a
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-08 02:04:17 +0500

    welcome message ui completed totaltasks,Pendingtasks, inprogressTasks and completedTasks UI completed

--- src/pages/Admin/Dashboard.jsx
+++ src/pages/Admin/Dashboard.jsx
@@ -5,6 +5,9 @@ import DashboardLayout from "../../components/layout/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
+import { addThousandsSeparator } from "../../utils/helper";
+import InfoCard from "../../components/Cards/InfoCard";
 
 const Dashboard = () => {
   useUserAuth();
@@ -23,7 +26,7 @@ const Dashboard = () => {
         API_PATHS.TASKS.GET_DASHBOARD_DATA,
       );
       if (response.data) {
-        setDashboardData;
+        setDashboardData(response.data);
       }
     } catch (error) {
       console.error(`Error fetching Users data: ${error}`);
@@ -34,7 +37,51 @@ const Dashboard = () => {
     getDashboardData();
   }, []);
 
-  return <DashboardLayout activeMenu={"Dashboard"}>Dashboard</DashboardLayout>;
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="card my-5">
+        <div>
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+          <InfoCard
+            label="Total Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.All || 0,
+            )}
+            color="bg-primary"
+          />
+          <InfoCard
+            label="Pending Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Pending || 0,
+            )}
+            color="bg-violet-500"
+          />
+          <InfoCard
+            label="In Progress Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.InProgress || 0,
+            )}
+            color="bg-cyan-500"
+          />
+          <InfoCard
+            label="Completed Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Completed || 0,
+            )}
+            color="bg-lime-500"
+          />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
