ed754c1fba7f133f1d2d467e1352e35c4ceacfbc
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-09 23:59:18 +0500

    createing create task

--- src/pages/Admin/CreateTask.jsx
+++ src/pages/Admin/CreateTask.jsx
@@ -51,7 +51,68 @@ const CreateTask = () => {
   const getTaskDetailsByID = async () => {};
   const deleteTask = async () => {};
 
-  return <DashboardLayout activeMenu="Create Task">CreateTask</DashboardLayout>;
+  return (
+    <DashboardLayout activeMenu="Create Task">
+      <div className="mt-5">
+        <div className="grid grid-cols-1 md:grid-cols-4 mt-4">
+          <div className="form-card col-span-3">
+            <div className=" flex items-center justify-between">
+              <h2 className="text-xl md:text-xl font-medium">
+                {taskId ? "Update Task" : "Create Task"}
+              </h2>
+              {taskId && (
+                <button
+                  className="flex items-center gap-1.5 text-[13px] font-medium text-rose-500 bg-reose-50 rounded px-2 py-1 border border-rose-100 hover:border-rose-300 cursor-pointer"
+                  onClick={() => setOpenDeleteAlert(true)}
+                >
+                  <LuTrash2 className="text-base" />
+                </button>
+              )}
+            </div>
+            <div className="mt-4">
+              <label className="text-xs font-medium text-slate-600">
+                Task Title
+              </label>
+
+              <input
+                placeholder="Create App UI"
+                className="form-input"
+                value={taskData.title}
+                onChange={({ target }) => {
+                  handleValueChange("title", target.value);
+                }}
+              />
+            </div>
+            <div className="mt-3">
+              <label className="text-xs font-medium text-slate-600">
+                Description
+              </label>
+
+              <textarea
+                placeholder="Describe Task"
+                className="form-input"
+                rows={4}
+                value={taskData.description}
+                onChange={({ target }) =>
+                  handleValueChange("description", target.value)
+                }
+              />
+            </div>
+            <div className="grid grid-cols-12 gap-4 mt-2">
+              <div className="col-sapn-6 md:col-span-4">
+                <label text>Priority</label>
+                <SelectDropDown
+                  options={PRIORITY_DATA}
+                  value={taskData.priority}
+                  onChange={(value) => handleValueChange("priority", value)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default CreateTask;
