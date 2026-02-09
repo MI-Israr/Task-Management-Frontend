658f203bcf5775895541d99d038bf154a0bb4fa1
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-09 22:51:17 +0500

    creating createTask, deleteTask, updateTask, handleSubmit, getTaskDetailsByID functions for CreateTask.jsx component

--- src/pages/Admin/CreateTask.jsx
+++ src/pages/Admin/CreateTask.jsx
@@ -6,8 +6,51 @@ import toast from "react-hot-toast";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { LuTrash2 } from "react-icons/lu";
+import { useState } from "react";
 
 const CreateTask = () => {
+  const location = useLocation();
+  const { taskId } = location.state || {};
+  const navigate = useNavigate();
+
+  const [taskData, setTaskData] = useState({
+    title: "",
+    description: "",
+    priority: "Low",
+    dueDate: null,
+    assignedTo: [],
+    todoChecklist: [],
+    attachments: [],
+  });
+
+  const [currentTask, setCurrentTask] = useState(null);
+
+  const [error, seterror] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState(false);
+
+  const handleValueChange = (key, value) => {
+    setTaskData((prevData) => ({ ...prevData, [key]: value }));
+  };
+
+  const clearData = () => {
+    setTaskData({
+      title: "",
+      description: "",
+      priority: "Low",
+      dueDate: null,
+      assignedTo: [],
+      todoChecklist: [],
+      attachments: [],
+    });
+  };
+
+  const createTask = async () => {};
+  const updateTask = async () => {};
+  const handleSubmit = async () => {};
+  const getTaskDetailsByID = async () => {};
+  const deleteTask = async () => {};
+
   return <DashboardLayout activeMenu="Create Task">CreateTask</DashboardLayout>;
 };
 
