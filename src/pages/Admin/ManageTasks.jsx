e3dd330ee4d9ea49b2ff77a7f257bc11534c692a
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-14 01:43:40 +0500

    implementing ManageTasks.jsx

--- src/pages/Admin/ManageTasks.jsx
+++ src/pages/Admin/ManageTasks.jsx
@@ -1,11 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DashboardLayout from "../../components/layout/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import { API_PATHS } from "../../utils/apiPaths";
+import axiosInstance from "../../utils/axiosInstance";
 
 const ManageTasks = () => {
   const [allTasks, setAllTasks] = useState([]);
-
-  const [tabs, settabs] = useState([]);
+  const [tabs, setTabs] = useState([]);
   const [filterStatus, setFilterStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  const getAllTasks = async () => {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: filterStatus === "All" ? "" : filterStatus,
+        },
+      });
+
+      setAllTasks(response.data?tasks?.length > 0 ? response.data.tasks : []);
+
+      const statusSummary = response.data?.statusSummary || {}
+      const statusArray = [
+        {label:"All", count: statusSummary.all || 0},
+        {label:"Pending", count: statusSummary.pendingTasks || 0},
+        {label:"In Progress", count: statusSummary.inProgressTasks || 0},
+        {label:"Completed", count: statusSummary.completed || 0}
+      ]
+      setTabs(statusArray)
+    } catch (error) {console.error(`Error fetching Users: ${error}`);
+  };
+
+  const handleClick = (taskData) => {
+    navigate(`/admin/create-task`, { state: { taskId: taskData._id } });
+  };
+
+  const handleDownloadReport = async () => {};
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+  }, [filterStatus]);
 
   return (
     <DashboardLayout activeMenu="Manage Tasks">
