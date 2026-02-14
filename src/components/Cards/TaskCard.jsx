7553979095024346fe4b07e96daf95ce64fbeebb
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-14 23:59:23 +0500

    created getStatusTagColor in TaskCard.jsx

--- src/components/Cards/TaskCard.jsx
+++ src/components/Cards/TaskCard.jsx
@@ -13,6 +13,17 @@ const TaskCard = ({
   updatedAt,
   onClick,
 }) => {
+  const getStatusTagColor = () => {
+    switch (status) {
+      case "In Progress":
+        return "text-cyan-500 bg-cyan-50 border border-cyan-500/10";
+      case "Completed":
+        return "text-lime-500 bg-lime-50 border border-lime-500/10";
+      default:
+        return "text-violet-500 bg-violet-50 border border-violet-500/10";
+    }
+  };
+
   return <div>TaskCard</div>;
 };
 
