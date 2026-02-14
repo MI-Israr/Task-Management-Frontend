20a98b6c2a61784fd42c5e90f95b082150c4e444
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-15 01:15:18 +0500

    creating TaskCard.jsx

--- src/components/Cards/TaskCard.jsx
+++ src/components/Cards/TaskCard.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Progress from "../Progress";
+import AvatarGroup from "../layout/AvatarGroup";
+import { LuPaperclip } from "react-icons/lu";
+import moment from "moment";
 
 const TaskCard = ({
   title,
@@ -24,7 +28,40 @@ const TaskCard = ({
     }
   };
 
-  return <div>TaskCard</div>;
+  const getPriorityTagColor = () => {
+    switch (priority) {
+      case "Low":
+        return "text-emerald-500 bg-emerald-50 border border-emerald-500/10";
+      case "Medium":
+        return "text-amber-500 bg-amber-50 border border-amber-500/10";
+      default:
+        return "text-rose-500 bg-rose-50 border border-rose-500/10";
+    }
+  };
+
+  return (
+    <div className="" onClick={onClick}>
+      <div className="">
+        <div
+          className={`text-[11px] font-medium ${getStatusTagColor()} px-4 py-0.5 rounded `}
+        >
+          {status}
+        </div>
+        <div
+          className={`text-[11px] font-medium ${getPriorityTagColor()} px-4 py-0.5 rounde`}
+        >
+          {priority} Priority
+        </div>
+      </div>
+      <div
+        className={`px-4 border-l-[3px] ${status === "In Progress" ? "border-cyan-500" : status === "Completed" ? "border-indigo-500" : "border-voilet-500"}`}
+      >
+        <p className="">{title}</p>
+        <p className="">{description}</p>
+        <p className="">Task Done: </p>
+      </div>
+    </div>
+  );
 };
 
 export default TaskCard;
