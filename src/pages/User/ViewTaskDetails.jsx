9c60ac51f748940bf52a36b1e0a3a3fa1893763d
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-18 02:17:13 +0500

    implemented updateTodoChecklist  functionality

--- src/pages/User/ViewTaskDetails.jsx
+++ src/pages/User/ViewTaskDetails.jsx
@@ -36,9 +36,33 @@ const ViewTaskDetails = () => {
       console.error(`Error fetching Task: ${error}`);
     }
   };
-  const updateTodoChecklist = async (index) => {};
-  
+  const updateTodoChecklist = async (index) => {
+    const todoChecklist = [...task?.todoChecklist];
+    const taskId = id;
+
+    if (todoChecklist && todoChecklist[index]) {
+      todoChecklist[index].completed = !todoChecklist[index].completed;
+
+      try {
+        const response = await axiosInstance.put(
+          API_PATHS.TASKS.UPDATE_TODO_CHECKLIST(taskId),
+          { todoChecklist },
+        );
+        if (response.status === 200) {
+          setTask(response.data?.task || task);
+        } else {
+          todoChecklist[index].completed = !todoChecklist[index].completed;
+        }
+      } catch (error) {
+        todoChecklist[index].completed = !todoChecklist[index].completed;
+      }
+    }
+  };
+
   const handleLinkClick = (link) => {
+    if (!/^https?:\/\//i.test(link)) {
+      link = "https://" + link;
+    }
     window.open(link, "_blank");
   };
 
