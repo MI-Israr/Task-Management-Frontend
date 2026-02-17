125f228cabe78d5001fbac56c5d3581acc044599
Muhammad  Israr <124520126+MI-Israr@users.noreply.github.com>
2026-02-17 23:02:38 +0500

    creating ViewTaskDetails.jsx component

--- src/pages/User/ViewTaskDetails.jsx
+++ src/pages/User/ViewTaskDetails.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ViewTaskDetails = () => {
+  const { id } = useParams();
+  const [task, setTask] = useState(null);
+
+  const getStatusTagColor = (status) => {
+    switch (status) {
+      case "in progress":
+        return "text-cyan-500 bg-cyan-50 border border-cyan-500/10";
+      case "completed":
+        return "text-lime-500 bg-lime-50 border border-lime-500/10";
+      default:
+        return "text-violet-500 bg-violet-50 border border-violet-500/10";
+    }
+  };
+
   return <div>ViewTaskDetails</div>;
 };
 
